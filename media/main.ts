9adf5036ac0dbbf742f2d1b8d611a6f819006b68
Fathony L <fathony@smart-leaders.net>
2024-10-01 17:47:22 +0700

    complete, but doesnt work

---
+++ media/main.ts
@@ -0,0 +1,15 @@
+export const pi = 3.14;
+
+// Self executing anonymous function
+// This is the main entry point
+(async () => {
+    //
+    const node = document.createElement('div')
+    node.innerHTML = ('soyuz')
+    document.body.appendChild(node)
+    setTimeout(() => {
+        document.getElementById('teaser')!.innerHTML = "<b>hola!</b>"
+        
+    }, 1000);
+    document.title='Ha Hola'
+})()
--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,205 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "yup" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('yup.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Yup!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('yup.start', () => {
+			YupPreviewPanel.createOrShow(context.extensionUri);
+		})
+	);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
+	return {
+		// Enable javascript in the webview
+		enableScripts: true,
+
+		// And restrict the webview to only loading content from our extension's `media` directory.
+		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'out', 'media')]
+	};
+}
+
+/**
+ * Manages cat coding webview panels
+ */
+class YupPreviewPanel {
+	/**
+	 * Track the currently panel. Only allow a single panel to exist at a time.
+	 */
+	public static currentPanel: YupPreviewPanel | undefined;
+
+	public static readonly viewType = 'yupPreview';
+
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionUri: vscode.Uri;
+	private _disposables: vscode.Disposable[] = [];
+
+	public static createOrShow(extensionUri: vscode.Uri) {
+		const column = vscode.window.activeTextEditor
+			? vscode.window.activeTextEditor.viewColumn
+			: undefined;
+
+		// If we already have a panel, show it.
+		if (YupPreviewPanel.currentPanel) {
+			YupPreviewPanel.currentPanel._panel.reveal(column);
+			return;
+		}
+
+		// Otherwise, create a new panel.
+		const panel = vscode.window.createWebviewPanel(
+			YupPreviewPanel.viewType,
+			'Cat Coding',
+			column || vscode.ViewColumn.Two,
+			getWebviewOptions(extensionUri),
+		);
+
+		YupPreviewPanel.currentPanel = new YupPreviewPanel(panel, extensionUri);
+	}
+
+	public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		YupPreviewPanel.currentPanel = new YupPreviewPanel(panel, extensionUri);
+	}
+
+	private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		this._panel = panel;
+		this._extensionUri = extensionUri;
+
+		// Set the webview's initial html content
+		this._update();
+
+		// Listen for when the panel is disposed
+		// This happens when the user closes the panel or when the panel is closed programmatically
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+		// Update the content based on view changes
+		this._panel.onDidChangeViewState(
+			e => {
+				if (this._panel.visible) {
+					this._update();
+				}
+			},
+			null,
+			this._disposables
+		);
+
+		// Handle messages from the webview
+		this._panel.webview.onDidReceiveMessage(
+			message => {
+				switch (message.command) {
+					case 'alert':
+						vscode.window.showErrorMessage(message.text);
+						return;
+				}
+			},
+			null,
+			this._disposables
+		);
+	}
+
+	public doRefactor() {
+		// Send a message to the webview webview.
+		// You can send any JSON serializable data.
+		this._panel.webview.postMessage({ command: 'refactor' });
+	}
+	
+	public teaser(lines: string){
+		this._panel.webview.postMessage({ command: 'teaser', lines });
+	}
+
+	public dispose() {
+		YupPreviewPanel.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+
+	private _update() {
+		const webview = this._panel.webview;
+		this._panel.webview.html = this._getHtmlForWebview(webview);
+
+		// Vary the webview's content based on where it is located in the editor.
+		// switch (this._panel.viewColumn) {
+		// 	case vscode.ViewColumn.Two:
+		// 		// this._updateForCat(webview, 'Compiling Cat');
+		// 		return;
+
+		// 	case vscode.ViewColumn.Three:
+		// 		// this._updateForCat(webview, 'Testing Cat');
+		// 		return;
+
+		// 	case vscode.ViewColumn.One:
+		// 	default:
+		// 		// this._updateForCat(webview, 'Coding Cat');
+		// 		return;
+		// }
+	}
+
+	// private _updateForCat(webview: vscode.Webview, catName: keyof typeof cats) {
+	// 	this._panel.title = catName;
+	// 	this._panel.webview.html = this._getHtmlForWebview(webview, cats[catName]);
+	// }
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+		// Local path to main script run in the webview
+		const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'out', 'media', 'main.js');
+
+		// And the uri we use to load this script in the webview
+		const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+
+		// Local path to css styles
+		const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'out', 'media', 'reset.css');
+		const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'out', 'media', 'vscode.css');
+
+		// Uri to load styles into webview
+		const stylesResetUri = webview.asWebviewUri(styleResetPath);
+		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+
+		// Use a nonce to only allow specific scripts to be run
+		const nonce = getNonce();
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<link href="${stylesResetUri}" rel="stylesheet">
+				<link href="${stylesMainUri}" rel="stylesheet">
+
+				<title>Cat Coding</title>
+			</head>
+			<body>
+				<h1 id="lines-of-code-counter">0</h1>
+
+				<pre id="teaser"></pre>
+
+				<script nonce="${nonce}" type="module" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+	}
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
