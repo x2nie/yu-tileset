c34b0f7c1a364bb97848c888f0900a1c37cb2a6d
Fathony L <fathony@smart-leaders.net>
2024-10-03 17:49:34 +0700

    display:grid. failed.

--- webview-ui/src/components/tile.ts
+++ webview-ui/src/components/tile.ts
@@ -6,8 +6,16 @@ export default class Tile extends Component {
     static template = xml/*html*/`
         <div class="tile">
             <div class="preview">
-                <!-- <pre t-out="props.value" /> -->
-                <span t-out="window.JSON.stringify(props.grid).replaceAll(',',', ')" />
+                <pre t-out="props.value" />
+                <!-- <span t-out="window.JSON.stringify(props.grid).replaceAll(',',', ')" /> -->
+                <t t-foreach="props.grid" t-as="plane" t-key="plane_index">
+                    <p t-attf-style="--cols:#{plane[0].length};">
+                        <t t-foreach="plane" t-as="y" t-key="y_index">
+                            <i t-foreach="y" t-as="x" t-key="x_index" t-att-c="x"/>
+                            <!-- <br/> -->
+                        </t>
+                    </p>
+                </t>
             </div>
 
             <h6 t-out="props.name" />
