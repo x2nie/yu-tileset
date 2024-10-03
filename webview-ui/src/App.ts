26255f9f0f962936c0aa32460d70127565de9056
Fathony L <fathony@smart-leaders.net>
2024-10-03 20:09:44 +0700

    Neighbor !

--- webview-ui/src/App.ts
+++ webview-ui/src/App.ts
@@ -3,12 +3,12 @@ import { parseXml } from "@rgrove/parse-xml";
 import { vscode } from "./utilities/vscode";
 // import './App.scss'
 import "./App.css";
-import Tile from "./components/tile";
+import { Tile, Neighbor } from "./components/tile";
 import { parseTileset } from "./xml";
 
 
 export default class App extends Component {
-    static components = {Tile}
+    static components = {Tile, Neighbor}
     // static template = xml`<h1>Hey Guys!</h1> <t t-call="greeting"/>`;
     // static template = xml`<h1>Hey Guys!</h1>`;
     state : any;
@@ -60,6 +60,25 @@ export default class App extends Component {
           });
     }
 
+    att2grid(att:string) { //eg. "Door"
+        let [rotate, tilename] = att.split(' ')
+        if(!tilename){
+            tilename = rotate;
+            rotate = ''
+        }
+        const tiles = this.state.tileset.children[1]
+        for (let i = 0; i < tiles.children.length; i++) {
+            if(tiles.children[i].attributes?.name == tilename) {
+                return {
+                    grid:tiles.children[i].grid,
+                    name:tiles.children[i].attributes.name,
+                    rotate
+                }
+            }
+            
+        }
+    }
+
     click_demo(){
         console.log('hello')
         vscode.postMessage({
@@ -82,8 +101,8 @@ App.template = xml`
         </t>
         <t t-if="section.name=='neighbors'" >
             <t t-foreach="section.children" t-as="neighbor" t-key="neighbor_index" t-if="neighbor.name=='neighbor'">
-                <!-- <Tile t-if="tile.name=='tile'" grid="tile.grid" t-props="tile.attributes"/> -->
-                 <pre t-out="window.JSON.stringify(neighbor.attributes)"/>
+                <Neighbor left="att2grid(neighbor.attributes.left)" right="att2grid(neighbor.attributes.right)" />
+                 <!-- <pre t-out="window.JSON.stringify(neighbor.attributes)"/> -->
             </t>
         </t>
     </t>
--- webview-ui/src/components/tile.ts
+++ webview-ui/src/components/tile.ts
@@ -2,25 +2,77 @@ import { Component, xml } from "@odoo/owl";
 
 import './tile.scss'
 
-export default class Tile extends Component {
+export class Tile extends Component {
     static template = xml/*html*/`
         <div class="tile">
-            <div class="preview">
+            <div class="preview-box">
                 <!-- <pre t-out="props.value" /> -->
                 <!-- <span t-out="window.JSON.stringify(props.grid).replaceAll(',',', ')" /> -->
-                <t t-foreach="props.grid" t-as="plane" t-key="plane_index">
-                    <p t-attf-style="--cols:#{plane[0].length};">
-                        <t t-foreach="plane" t-as="y" t-key="y_index">
-                            <i t-foreach="y" t-as="x" t-key="x_index" t-att-title="x"/>
-                        </t>
-                    </p>
-                </t>
+                <div class="cube">
+                    <t t-foreach="props.grid" t-as="plane" t-key="plane_index">
+                        <p t-attf-style="--cols:#{plane[0].length};">
+                            <t t-foreach="plane" t-as="y" t-key="y_index">
+                                <i t-foreach="y" t-as="x" t-key="x_index" t-att-title="x"/>
+                            </t>
+                        </p>
+                    </t>
+                </div>
             </div>
 
             <h4 t-out="props.name" />
-
-
-
         </div>
     `
+}
+
+export class Neighbor extends Component {
+    static template0 = xml/*html*/`
+        <pre t-out="window.JSON.stringify(props, null, 4)"/>
+    `
+    static template = xml/*html*/`
+        <div class="tile">
+            <div class="preview-box">
+                <!-- <pre t-out="props.value" /> -->
+                <!-- <span t-out="window.JSON.stringify(props.grid).replaceAll(',',', ')" /> -->
+                <div class="cube" t-att-style="rotate2style(props.left.rotate)">
+                    <t t-foreach="props.left.grid" t-as="plane" t-key="plane_index">
+                        <p t-attf-style="--cols:#{plane[0].length};">
+                            <t t-foreach="plane" t-as="y" t-key="y_index">
+                                <i t-foreach="y" t-as="x" t-key="x_index" t-att-title="x"/>
+                            </t>
+                        </p>
+                    </t>
+                </div>
+                <div class="cube" t-att-style="rotate2style(props.right.rotate)">
+                    <t t-foreach="props.right.grid" t-as="plane" t-key="plane_index">
+                        <p t-attf-style="--cols:#{plane[0].length};">
+                            <t t-foreach="plane" t-as="y" t-key="y_index">
+                                <i t-foreach="y" t-as="x" t-key="x_index" t-att-title="x"/>
+                            </t>
+                        </p>
+                    </t>
+                </div>
+            </div>
+
+            <h4>
+                <t t-out="props.left.name"/> - 
+                <t t-out="props.right.name"/>
+            </h4>
+        </div>
+    `
+
+    rotate2style(rotates:string) {
+        let z = 0;
+        let x = 0;
+        let y = 0;
+        rotates.split('').forEach(rotate => {
+            switch (rotate) {
+                case 'z':
+                    z += 90
+                    break;
+                default:
+                    break;
+            }
+        })
+        return `transform: rotateZ(${z}deg);`
+    }
 }
